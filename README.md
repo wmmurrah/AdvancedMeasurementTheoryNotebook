baea102714c42f9672211dccc4bbe3f1ff15b582
wmmurrah <william.murrah@gmail.com>
2021-08-16 13:03:54 -0500

    Add factor analysis material from DAIII course

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This is a Course Notebook for Advanced Measurement Theory (ERMA 8350). It includes notes, code, graphics, and other project material from the 
+This is a Course Notebook for Advanced Measurement Theory (ERMA 8350). It includes notes, code, graphics, and other project material related to the course.
+
---
+++ code/simulate_CTTdata.R
@@ -0,0 +1,32 @@
+#************************************************************************
+# Title: simulate_CTTdata
+# Author: William Murrah
+# Description: Simulate data to demonstrate CTT and reliability
+# Created: Monday, 09 August 2021
+# R version: R version 4.1.0 (2021-05-18)
+# Project(working) directory: /Users/wmm0017/Projects/Courses/AdvancedMeasurementTheoryNotebook
+#************************************************************************
+
+simx <- function(truescore, sigmax = 1) {
+  x <- rnorm(18, truescore, sigmax)
+  return(round(x))
+}
+id <- rep(1:6, each = 3)
+Tau <- rep(rep(4:6, each = 3),2)
+set.seed(20210805)
+CTTdata <- data.frame(
+  id = id,
+  time = rep(1:3, 6),
+  x1 = simx(Tau),
+  x2 = simx(Tau),
+  x3 = simx(Tau),
+  x4 = simx(Tau),
+  x5 = simx(Tau),
+  x6 = simx(Tau),
+  x7 = simx(Tau),
+  x8 = simx(Tau),
+  x9 = simx(Tau),
+  x10 = simx(Tau),
+  Tau = Tau
+)
+rm(id, Tau, simx)
